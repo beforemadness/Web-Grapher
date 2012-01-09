29d52fd0abf3bd5a143d4fe7ef861fdbbd7c708b
Shyam Prakash <sparks.ks@gmail.com>
2012-01-09 13:22:43 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grapher::Application.config.secret_token = 'd42c6de4e67bcfacd41fc4712f192ec28c11efff46680ff09222f0ecb1393f5334979fae4f9e1a5ea341a434608ea60fcbc5cb0dad1bead66b91a481a70d172b'
